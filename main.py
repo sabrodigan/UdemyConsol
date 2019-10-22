9a0224dd7d42645c5d3388423d1796f569be63f6
Stephen Brodigan <sabrodigan@gmail.com>
2019-10-22 08:08:48 +0000

    Added  code to identify the duplicate int's in a list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ name = 'Joe'
 print('Hi, %s.' % name)
 
 mylist = [1,2,3,4,4,5]
-another_list = [6,7,8,9,10,10,11]
+another_list = [6,7,8,9,10,10,11,16,21,34,56,21,44,23,44]
 
 print (mylist)
 print (another_list)
@@ -36,7 +36,7 @@ print (final_list)
 final_list.remove(4)
 print (final_list)
 
-final_list.append ('Odd Number')
+final_list.append ('\nOdd Number')
 print (final_list)
 
 final_list.pop()
@@ -44,12 +44,35 @@ print (final_list)
 
 
 # Removing items in a list using index numbers with .pop
-item = int (input("Which index value do you want to remove?"))
+item = int (input("\nWhich index value do you want to remove? "))
+print (f'\nYou entered index number {item} and we are going to remove it')
 
+x = final_list[item]
 
-print (final_list[item])
+item_range = item + 2
+print (f'We are going to print a range from index {item, item_range}')
+print (f'\nFinal list, range is {final_list[item:item_range]}')
+
+print (f'The item we are removing is {final_list[item]}')
 final_list.pop(item)
-print (f'We removed {item} from the list')
+
+print (f'\nWe removed {item} from the list with a value {x}')
 print (final_list)
 
+
+# trying to find duplicates in final_list
+
+print ('\n More attempts at duplicates here')
+
+dupItems = []
+uniqItems = {}
+for x in final_list:
+   if x not in uniqItems:
+      uniqItems[x] = 1
+   else:
+      if uniqItems[x] == 1:
+         dupItems.append(x)
+      uniqItems[x] += 1
+print(f'Duplicate numbers in final_list {dupItems}')
+  
 
